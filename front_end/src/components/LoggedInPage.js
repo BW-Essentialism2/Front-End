f25a46afd53c9bcf5bcbddf8d331c99a5fa67305
Tiffany Crosby <tiffany.crosby65@gmail.com>
2020-05-26 18:57:22 -0400

    waiting on end points, state for options

--- front_end/src/components/LoggedInPage.js
+++ front_end/src/components/LoggedInPage.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Route, Link } from 'react-router-dom';
 
-// import {axiosWithAuth} from '../utils/axiosWithAuth'
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 export const LoggedInPage = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/users')
+      .then((response) =>
+        console.log('response from GET request LOGGEDINPAGE', response)
+      )
+      .catch((error) =>
+        console.log('Error from GET request LOGGEDINPAGE', error)
+      );
+  }, [user]);
+
   return (
-    <div>
-      <h1>This is the logged in page!!! Yippie!</h1>
+    <div className="login">
+      <h1>Thank You for Logging In {user.firstname}</h1>
+      {/* {user.values.map(value => {
+          user.values:true ? <div>Current Choice: {value}</div> : null
+      })} */}
+      <button className="editBTN" onClick={() => console.log('hello')}>
+        Make/Edit Your Choices
+      </button>
+      <button
+        className="deactivateBTN"
+        onClick={() => console.log('I am deactivated')}
+      >
+        De-activate Account
+      </button>
     </div>
   );
 };
--- front_end/src/utils/axiosWithAuth.js
+++ front_end/src/utils/axiosWithAuth.js
@@ -1,14 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const axiosWithAuth = () => {
-    const token = localStorage.getItem('token')
-    return axios.create({
-        headers: {
-            authorization: token
-        }
-        baseUrl: 'https://essentialism-3.herokuapp.com/api'
-    })
-}
-
-//WHICH STORAGE DO I USE??????????
-//Make sure headers format to get token is correct!!!!!!!
+  const token = localStorage.getItem('token');
+  return axios.create({
+    headers: {
+      authorization: token,
+    },
+    baseUrl: 'https://essentialism-3.herokuapp.com/api',
+  });
+};
