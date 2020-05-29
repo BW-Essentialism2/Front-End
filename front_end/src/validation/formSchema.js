d39cf63b7801d4e8f52af87028c04a4604f4dc35
Karen Lei <karenwinnielei@gmail.com>
2020-05-29 10:18:13 -0700

    work on validation

--- front_end/src/validation/formSchema.js
+++ front_end/src/validation/formSchema.js
@@ -2,17 +2,32 @@ import * as yup from 'yup'
 
 const formSchema = yup.object().shape({
     value1: yup.string()
-    .trim()
-    .min(5, 'Response must be at least 50 characters long')
-    .required('Response is a required field'),
+        .trim()
+        .min(50, 'Response must be at least 50 characters long')
+        .required('Response is a required field'),
     value2: yup.string()
-    .trim()
-    .min(5, 'Response must be at least 50 characters long')
-    .required('Response is a required field'),
+        .trim()
+        .min(50, 'Response must be at least 50 characters long')
+        .required('Response is a required field'),
     value3: yup.string()
-    .trim()
-    .min(5, 'Response must be at least 50 characters long')
-    .required('Response is a required field'),
+        .trim()
+        .min(50, 'Response must be at least 50 characters long')
+        .required('Response is a required field'),
+    // firstname: yup.string()
+    //     .trim()
+    //     .min(2, 'First name must be at least two characters long')
+    //     .required('First name is a required field'),
+    // lastname: yup.string()
+    //     .trim()
+    //     .min(2, 'Last name must be at least two characters long')
+    //     .required('Last name is a required field'),
+    // email: yup.string()
+    //     .email('Email must be a valid email address')
+    //     .required('Email is a required field'),
+    // password: yup.string()
+    //     .trim()
+    //     .required('Password is a required field'),
+
     
 })
 
